c9c9e4065bd69691fb84b78346c03dcd0984bae3
Humberto Borba <humberos@gmail.com>
2014-03-26 15:06:39 -0300

    p1 [1/3]: bring back hybrid gsm install package
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: If57fb8ea98a53004f97430fc0001eea261a75dfd

--- Android.mk
+++ Android.mk
@@ -20,3 +20,11 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),p1)
   include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
+
+ifeq ($(TARGET_DEVICE),p1l)
+  include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
+
+ifeq ($(TARGET_DEVICE),p1n)
+  include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
